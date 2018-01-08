30a1c976be4676cc39bb0a76f2ece5845602f1b7
A. Mioranza <amioranza@mdcnt.ninja>
2018-01-07 23:42:50 -0200

    Changed the fixed stats variables on haproxy template to attributes

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -1,47 +1,50 @@
 # Create haproxy config directory on the docker host
 directory '/etc/haproxy' do
-    owner 'root'
-    group 'root'
-    mode '0755'
+    owner  'root'
+    group  'root'
+    mode   '0755'
     action :create
 end
 
 # Create ssl certificates directory on the docker host
 directory '/etc/ssl' do
-    owner 'root'
-    group 'root'
-    mode '0755'
+    owner  'root'
+    group  'root'
+    mode   '0755'
     action :create
 end
 
 # Create the haproxy.cfg file based on the template attached to the cookbook
 template '/etc/haproxy/haproxy.cfg' do
     source 'etc/haproxy/haproxy.cfg.erb'
-    owner 'root'
-    group 'root'
-    mode '0644'
+    owner  'root'
+    group  'root'
+    mode   '0644'
+    variables({ :stats_port => node['haproxy']['stats']['port'],
+                :stats_user => node['haproxy']['stats']['user'],
+                :stats_pass => node['haproxy']['stats']['pass'] })
 end
 
 # Create the cert.pem file based on the template attached to the cookbook
 template '/etc/ssl/cert.pem' do
     source 'etc/ssl/cert.pem.erb'
-    owner 'root'
-    group 'root'
-    mode '0644'
+    owner  'root'
+    group  'root'
+    mode   '0644'
 end
 
 # Install docker release form Rancher, basically the same from the official repo
 docker_installation_script 'Install Docker from Rancher repo' do
-    repo 'main'
+    repo       'main'
     script_url 'https://releases.rancher.com/install-docker/17.03.sh'
-    action :create
+    action     :create
 end
 
 # Create a network to plug the containers, using custom networks does not require links between containers
 docker_network 'internal' do
-    subnet '192.168.88.0/24'
+    subnet  '192.168.88.0/24'
     gateway '192.168.88.1'
-    action :create
+    action  :create
 end
 
 # Delete existing container to deploy again
@@ -52,19 +55,19 @@ end
 
 # Pull the image from docker hub
 docker_image "jenkins" do
-    repo 'jenkins/jenkins'
-    tag 'lts'
+    repo   'jenkins/jenkins'
+    tag    'lts'
     action :pull
 end
 
 # Deploy jenkins CI to test the setup, can be any other image
 docker_container 'jenkins' do
-    repo 'jenkins/jenkins'
-    tag 'lts'
-    port ['8080:8080', '50000:50000']
-    privileged false
+    repo         'jenkins/jenkins'
+    tag          'lts'
+    port         ['8080:8080', '50000:50000']
+    privileged   false
     network_mode 'internal'
-    action :run_if_missing
+    action       :run_if_missing
 end
 
 # Delete existing container to deploy again
@@ -75,18 +78,18 @@ end
 
 # Pull the image from docker hub
 docker_image "haproxy_lb" do
-    repo 'haproxy'
-    tag 'alpine'
+    repo   'haproxy'
+    tag    'alpine'
     action :pull
 end
 
 # Deploy haproxy alpine flavor to keep everything as slim as possible
 docker_container 'haproxy_lb' do
-    repo 'haproxy'
-    tag 'alpine'
-    port ['80:80', '443:443','9000:9000']
-    volumes ['/etc/haproxy:/usr/local/etc/haproxy', '/etc/ssl:/usr/local/etc/ssl']
-    privileged false
+    repo         'haproxy'
+    tag          'alpine'
+    port         ["80:80", "443:443","#{node['haproxy']['stats']['port']}:#{node['haproxy']['stats']['port']}"]
+    volumes      ['/etc/haproxy:/usr/local/etc/haproxy', '/etc/ssl:/usr/local/etc/ssl']
+    privileged   false
     network_mode 'internal'
-    action :run_if_missing
+    action       :run_if_missing
 end
