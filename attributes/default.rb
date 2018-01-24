3bde1a2e4e9760ad05929addbbabbf1423d4b58c
A. Mioranza <amioranza@mdcnet.ninja>
2018-01-24 00:08:28 -0200

    Minor changes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
-default['haproxy']['stats']['port'] = 28888
+default['haproxy']['stats']['port'] = 9000
 default['haproxy']['stats']['user'] = 'haproxy_adm'
-default['haproxy']['stats']['pass'] = 'H@Pr0xy##ADM'
+default['haproxy']['stats']['pass'] = 'H@Pr0xy_-ADM'
